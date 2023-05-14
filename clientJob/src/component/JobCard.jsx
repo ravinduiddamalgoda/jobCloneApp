4c6724db47dec961f8e96d8d5c76a8e83c3312ee
IT21209352 <it21209352@my.sliit.lk>
2023-05-14 19:42:41 +0530

    styled user and job card

--- clientJob/src/component/JobCard.jsx
+++ clientJob/src/component/JobCard.jsx
@@ -2,82 +2,98 @@ import { Box, makeStyles } from "@material-ui/core";
 import { Card, Container, List, ListItem, Typography } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
-
-
-    boxStyle:{
-        border: '1px solid',
-        [theme.breakpoints.up('900')]: {
-            width: '600px',
-        },
-        [theme.breakpoints.down('900')]: {
-            width: '300px',
-        },
-        [theme.breakpoints.down('600')]: {
-            width: '300px',
-        }, 
-        // background: '#4EB5FF',
-        // marginLeft: '15%',
-        // marginRight: '15%',
-        borderRadius: '0px',
-        marginTop:'3%',
+  card: {
+    borderRadius: '16px',
+    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    padding: '16px',
+    transition: 'box-shadow 0.3s ease',
+    '&:hover': {
+      boxShadow: '0px 8px 30px rgba(0, 0, 0, 0.15)',
     },
-
-    title1: {
-        flexDirection: 'colomn', 
-        flex: '1',
-        [theme.breakpoints.up('900')]: {
-            flexDirection: 'row',
-        },
-        [theme.breakpoints.down('900')]: {
-            flexDirection: 'row',
-        },
-        [theme.breakpoints.down('600')]: {
-            flexDirection: 'row',
-        }, 
-
-    }, 
-    title2:{
-        paddingTop: '2px', 
-    } ,
-
-    liStyleSkill:{
-        paddingLeft: '2px',
-        listStyle: 'square',
-        paddingTop:'2px',  
-    }
-
+    marginTop:"4rem"
+  },
+  titleContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '8px',
+  },
+  company: {
+    color: "#017143",
+    fontWeight: '600',
+    marginRight: '8px',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  jobTitle: {
+    fontWeight: '600',
+  },
+  description: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: '14px',
+    marginBottom: '16px',
+  },
+  skills: {
+    marginBottom: '8px',
+    fontStyle: 'italic',
+  },
+  skill: {
+    color: "#28a745",
+    fontWeight: '600',
+    marginRight: '8px',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  detail: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: '14px',
+  },
+  bullet: {
+    display: 'inline-block',
+    marginRight: '8px',
+    '&::before': {
+      content: '""',
+      display: 'inline-block',
+      width: '8px',
+      height: '8px',
+      borderRadius: '50%',
+      backgroundColor: "#017143",
+    },
+  },
 }));
 
-export function JobCard(props){
-    const className = useStyles();
-    return(<>
-        <Card className={className.boxStyle}>
-            <Container sx={{ display: 'flex'}}>
-            <Typography variant="h5" className={className.title1}>{props.company}</Typography>
-                <Typography variant="h5" className={className.title2}>{props.jobTitle}</Typography>
-               
-            </Container>
-            <br />
-            
-            <Container>
-                
-                <Typography>{props.description}</Typography>
-                <Typography>Skills that You must have </Typography>
-               
-                <List dense={true}>
-                    {props.skill.map(item =>(
-                        <ListItem key = {item} className={className.liStyleSkill}>{item}</ListItem>
-                    ))}
-                </List>
-                <Typography>Job Time : {props.jobStatus}</Typography>
-               <Typography>Job Type : {props.jobType}</Typography>
-               <Typography>Job Field : {props.feild}</Typography>
-               <Typography>Job Field : {props.position}</Typography>
-               <br />
-            </Container>
-
-            
-        </Card>
-    </>);
-
+export function JobCard(props) {
+  const classes = useStyles();
+  
+  return (
+    <Card className={classes.card}>
+      <Container className={classes.titleContainer}>
+        <a href={props.companyUrl} className={classes.company}>{props.company}</a>
+        <Typography variant="body2" className={classes.jobTitle}>{props.jobTitle}</Typography>
+      </Container>
+      <Typography variant="body1" className={classes.description}>{props.description}</Typography>
+      <Container className={classes.skills}>
+        <Typography variant="body2" >Skills :</Typography>
+        <List dense>
+          {props.skill.map(item =>(
+            <ListItem key={item} className={classes.detail}>
+              <span className={classes.bullet}></span>
+              {item}
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+      <Typography variant="body2" className={classes.detail}>Job Time: {props.jobStatus}</Typography>
+      <Typography variant="body2" className={classes.detail}>Job Type: {props.jobType}</Typography>
+      <Typography variant="body2" className={classes.detail}>Job Field: {props.field}</Typography>
+      <Typography variant="body2" className={classes.detail}>Position: {props.position}</Typography>
+    </Card>
+  );
 }
